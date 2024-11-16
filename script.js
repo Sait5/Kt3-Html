34ff23dfe4ee3273661bcc37bc947a159ed0464d
Sait5 <balaevs134@gmail.com>
2024-11-16 12:43:37 +0300

    van

---
+++ script.js
@@ -0,0 +1,5 @@
+
+document.getElementById('burgerMenu').addEventListener('click', function () {
+    const menu = document.getElementById('menu');
+    menu.classList.toggle('active'); // Добавляет или убирает класс active для отображения меню
+});
